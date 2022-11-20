70b913b8754ff1319e42cdee55f478df8bcc898f
Yaya-Dev-Box <yayadevbox@gmail.com>
2022-11-20 16:55:34 +0100

    Implement models detection from camera image instead of continuous video

--- lite/examples/image_segmentation/android/app/src/main/java/org/tensorflow/lite/examples/imagesegmentation/fragments/CameraFragment.kt
+++ lite/examples/image_segmentation/android/app/src/main/java/org/tensorflow/lite/examples/imagesegmentation/fragments/CameraFragment.kt
@@ -17,26 +17,23 @@
 package org.tensorflow.lite.examples.imagesegmentation.fragments
 
 import android.annotation.SuppressLint
-import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.media.ExifInterface
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.Toast
-import androidx.camera.core.Preview
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.AspectRatio
-import androidx.camera.core.ImageProxy
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.content.ContextCompat
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.*
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
+import coil.load
+import com.github.dhaval2404.imagepicker.ImagePicker
+import kotlinx.coroutines.delay
 import org.tensorflow.lite.examples.imagesegmentation.ImageSegmentationHelper
 import org.tensorflow.lite.examples.imagesegmentation.ImageSegmentationHelper.SegmentationListener
 import org.tensorflow.lite.examples.imagesegmentation.OverlayView
@@ -44,8 +41,7 @@ import org.tensorflow.lite.examples.imagesegmentation.OverlayView.ColorLabel
 import org.tensorflow.lite.examples.imagesegmentation.R
 import org.tensorflow.lite.examples.imagesegmentation.databinding.FragmentCameraBinding
 import org.tensorflow.lite.task.vision.segmenter.Segmentation
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+
 
 class CameraFragment : Fragment(), SegmentationListener {
 
@@ -61,14 +57,7 @@ class CameraFragment : Fragment(), SegmentationListener {
 
     private lateinit var imageSegmentationHelper: ImageSegmentationHelper
     private lateinit var bitmapBuffer: Bitmap
-    private var preview: Preview? = null
-    private var imageAnalyzer: ImageAnalysis? = null
-    private var camera: Camera? = null
-    private var cameraProvider: ProcessCameraProvider? = null
     private val labelsAdapter: ColorLabelsAdapter by lazy { ColorLabelsAdapter() }
-
-    /** Blocking camera operations are performed using this executor */
-    private lateinit var cameraExecutor: ExecutorService
 
     override fun onResume() {
         super.onResume()
@@ -83,9 +72,6 @@ class CameraFragment : Fragment(), SegmentationListener {
     override fun onDestroyView() {
         _fragmentCameraBinding = null
         super.onDestroyView()
-
-        // Shut down our background executor
-        cameraExecutor.shutdown()
     }
 
     override fun onCreateView(
@@ -107,167 +93,57 @@ class CameraFragment : Fragment(), SegmentationListener {
             imageSegmentationListener = this
         )
 
-        // Initialize our background executor
-        cameraExecutor = Executors.newSingleThreadExecutor()
-
-        // Wait for the views to be properly laid out
-        fragmentCameraBinding.viewFinder.post {
-            // Set up the camera and its use cases
-            setUpCamera()
-        }
-
-        // Attach listeners to UI control widgets
-        initBottomSheetControls()
-
         with(fragmentCameraBinding.recyclerviewResults) {
             adapter = labelsAdapter
             layoutManager = GridLayoutManager(requireContext(), 3)
         }
 
-        fragmentCameraBinding.overlay.setOnOverlayViewListener(object :
-            OverlayView.OverlayViewListener {
+        fragmentCameraBinding.overlay.setOnOverlayViewListener(object : OverlayView.OverlayViewListener {
             override fun onLabels(colorLabels: List<ColorLabel>) {
                 // update label at here
                 labelsAdapter.updateResultLabels(colorLabels)
             }
         })
+
+        setOnClickListeners()
     }
 
-    private fun initBottomSheetControls() {
-        // When clicked, decrease the number of threads used for segmentation
-        fragmentCameraBinding.bottomSheetLayout.threadsMinus.setOnClickListener {
-            if (imageSegmentationHelper.numThreads > 1) {
-                imageSegmentationHelper.numThreads--
-                updateControlsUi()
-            }
-        }
-
-        // When clicked, increase the number of threads used for segmentation
-        fragmentCameraBinding.bottomSheetLayout.threadsPlus.setOnClickListener {
-            if (imageSegmentationHelper.numThreads < 4) {
-                imageSegmentationHelper.numThreads++
-                updateControlsUi()
-            }
-        }
-
-        // When clicked, change the underlying hardware used for inference. Current options are CPU
-        // GPU, and NNAPI
-        fragmentCameraBinding.bottomSheetLayout.spinnerDelegate.setSelection(0, false)
-        fragmentCameraBinding.bottomSheetLayout.spinnerDelegate.onItemSelectedListener =
-            object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
-                ) {
-                    imageSegmentationHelper.currentDelegate = position
-                    updateControlsUi()
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                    /* no op */
-                }
-            }
-    }
-
-    // Update the values displayed in the bottom sheet. Reset segmenter.
-    private fun updateControlsUi() {
-        fragmentCameraBinding.bottomSheetLayout.threadsValue.text =
-            imageSegmentationHelper.numThreads.toString()
-
-        // Needs to be cleared instead of reinitialized because the GPU
-        // delegate needs to be initialized on the thread using it when applicable
-        imageSegmentationHelper.clearImageSegmenter()
-        fragmentCameraBinding.overlay.clear()
-    }
-
-    // Initialize CameraX, and prepare to bind the camera use cases
-    private fun setUpCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-        cameraProviderFuture.addListener(
-            {
-                // CameraProvider
-                cameraProvider = cameraProviderFuture.get()
-
-                // Build and bind the camera use cases
-                bindCameraUseCases()
-            },
-            ContextCompat.getMainExecutor(requireContext())
-        )
-    }
-
-    // Declare and bind preview, capture and analysis use cases
-    @SuppressLint("UnsafeOptInUsageError")
-    private fun bindCameraUseCases() {
-
-        // CameraProvider
-        val cameraProvider =
-            cameraProvider ?: throw IllegalStateException("Camera initialization failed.")
-
-        // CameraSelector - makes assumption that we're only using the back camera
-        val cameraSelector =
-            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-
-        // Preview. Only using the 4:3 ratio because this is the closest to our models
-        preview =
-            Preview.Builder()
-                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-                .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
-                .build()
-
-        // ImageAnalysis. Using RGBA 8888 to match how our models work
-        imageAnalyzer =
-            ImageAnalysis.Builder()
-                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-                .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-                .build()
-                // The analyzer can then be assigned to the instance
-                .also {
-                    it.setAnalyzer(cameraExecutor) { image ->
-                        if (!::bitmapBuffer.isInitialized) {
-                            // The image rotation and RGB image buffer are initialized only once
-                            // the analyzer has started running
-                            bitmapBuffer = Bitmap.createBitmap(
-                                image.width,
-                                image.height,
-                                Bitmap.Config.ARGB_8888
-                            )
-                        }
-
-                        segmentImage(image)
-                    }
-                }
-
-        // Must unbind the use-cases before rebinding them
-        cameraProvider.unbindAll()
-
-        try {
-            // A variable number of use-cases can be passed here -
-            // camera provides access to CameraControl & CameraInfo
-            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
-
-            // Attach the viewfinder's surface provider to preview use case
-            preview?.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
-        } catch (exc: Exception) {
-            Log.e(TAG, "Use case binding failed", exc)
-        }
-    }
-
-    private fun segmentImage(image: ImageProxy) {
+    private fun segmentImage(bitmap: Bitmap,rotationInDegrees : Int) {
         // Copy out RGB bits to the shared bitmap buffer
-        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+        bitmapBuffer = Bitmap.createBitmap(bitmap)
 
-        val imageRotation = image.imageInfo.rotationDegrees
         // Pass Bitmap and rotation to the image segmentation helper for processing and segmentation
-        imageSegmentationHelper.segment(bitmapBuffer, imageRotation)
+        imageSegmentationHelper.segment(bitmapBuffer, rotationInDegrees)
     }
 
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        imageAnalyzer?.targetRotation = fragmentCameraBinding.viewFinder.display.rotation
+    private val startForCapturePhotoResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        val image = it.data?.data ?: return@registerForActivityResult
+        _fragmentCameraBinding?.imageView?.load(image) { allowHardware(false) }
+
+        val exif = ExifInterface(image.path.orEmpty())
+        val rotation: Int = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+        val rotationInDegrees: Int = exifToDegrees(rotation)
+
+        lifecycleScope.launchWhenStarted {
+            delay(1000)
+            val bitmap = _fragmentCameraBinding?.imageView?.drawable?.toBitmap(config = Bitmap.Config.ARGB_8888)
+            segmentImage(bitmap ?: return@launchWhenStarted,rotationInDegrees)
+        }
+    }
+
+    private fun exifToDegrees(exifOrientation: Int): Int {
+        return when (exifOrientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> 90
+            ExifInterface.ORIENTATION_ROTATE_180 -> 180
+            ExifInterface.ORIENTATION_ROTATE_270 -> 270
+            else -> 0
+        }
+    }
+
+    private fun setOnClickListeners() {
+        _fragmentCameraBinding?.cameraBtn?.setOnClickListener { view ->
+            ImagePicker.with(this).cameraOnly().createIntent { startForCapturePhotoResult.launch(it) }
+        }
     }
 
     // Update UI after objects have been segment. Extracts original image height/width
@@ -279,9 +155,6 @@ class CameraFragment : Fragment(), SegmentationListener {
         imageWidth: Int
     ) {
         activity?.runOnUiThread {
-            fragmentCameraBinding.bottomSheetLayout.inferenceTimeVal.text =
-                String.format("%d ms", inferenceTime)
-
             // Pass necessary information to OverlayView for drawing on the canvas
             fragmentCameraBinding.overlay.setResults(
                 results,
